c9291f535cfca7b478f66ff973b1d3d2d0b85cbc
Mark Hoad <markdhoad@gmail.com>
2015-11-26 20:25:11 -0500

    Add new test to ensure user logging in with incorrect details works as expected

--- spec/features/accounts/sign_in_spec.rb
+++ spec/features/accounts/sign_in_spec.rb
@@ -2,15 +2,24 @@ require 'rails_helper'
 describe "The signin process", :type => :feature do
   before :each do
     @account = FactoryGirl.create(:account)
+    visit '/accounts/sign_in'
   end
 
-  it "signs me in with correct login details" do
-    visit '/accounts/sign_in'
+  scenario "signs me in with correct login details" do
     within("#new_account") do
       fill_in 'Email', :with => @account.email
       fill_in 'Password', :with => @account.password
     end
     click_button 'Log in'
     expect(page).to have_content I18n.t 'devise.sessions.signed_in'
+  end
+
+  scenario "signs me in with incorrect login details" do
+    within("#new_account") do
+      fill_in 'Email', :with => @account.email
+      fill_in 'Password', :with => "incorrect_password"
+    end
+    click_button 'Log in'
+    expect(page).to_not have_content I18n.t 'devise.sessions.invalid'
   end
 end
